9f46faf885e4d68b97c6b5bbdf31ef60c5515eb2
Alex <alex_zelea@yahoo.com>
2020-10-06 14:58:19 +0100

    xazxc
    zxczx

--- 7.price-section/pricing.php
+++ 7.price-section/pricing.php
@@ -6,51 +6,50 @@ echo '
            <h3>Packages for every budget!</h3>
         <div class="container group">
             <div class="grid-1-5">
-                <p class="price-package">Basic</p>
-                <p  class="price-in-pounds"><span class="uppercase">£299</span></p>
+                <p class="price-package">Affordable</p>
+                <p  class="price-in-pounds"><span class="uppercase">£249</span></p>
                 <p>1 Page</p>
                 <ul>
                     <li>Business Email</li>
-                    <li>Free .co.uk or .com domain name</li>
+                    <li>Domain name .co.uk or .com</li>
+                    <li>Hosting 12 months</li>
                     <li>Free SSL Certificate</li>
                     <li>Mobile Responsive</li>
-                    <li>Hosting £80/year</li>
                 </ul>
                 <a class="button-pricing" href="tel:07794922161">Call us</a>
             </div>
             <div class="grid-1-5">
                 <p class="price-package">Standard</p>
-                <p class="price-in-pounds"><span class="uppercase">£499</span></p>
-                <p>Up to 5 pages</p>
+                <p class="price-in-pounds"><span class="uppercase">£349</span></p>
+                <p> 1 - 3 pages</p>
                 <ul>
-                    <li>Everything from Basic</li>
+                    <li>Everything from Affordable </li>
                     <li>Google Analytics</li>
-                    <li>Digital Visibility</li>
-                    <li>1 year Free Hosting</li>
-                    <li>Slideshow Animation</li>
+                    <li>Appear on Google Maps</li>
+                    <li>Secure contact form</li>
                 </ul>
                 <a class="button-pricing" href="tel:07794922161">Call us</a>
             </div>
             <div class="grid-1-5">
                 <p class="price-package">Premium &amp; SEO</p>
-                <p class="price-in-pounds"><span class="uppercase">£899</span></p>
-                <p>Up to 10 pages</p>
+                <p class="price-in-pounds"><span class="uppercase">£449</span></p>
+                <p>Up to 5 pages</p>
                 <ul>
-                    <li>Everything from Standard</li>
-                    <li>85% On-page SEO Guaranteed</li>
+                    <li>Everything from Affordable &amp; Standard</li>
                     <li>Page submission to Google &amp; Bing</li>
+                    <li>Digital Visibility</li>
                     <li>2 years Free Hosting</li>
-                    <li>Special Animations</li>
+                    <li>Slideshow Animation</li>
 
                 </ul>
                 <a class="button-pricing" href="tel:07794922161">Call us</a>
             </div>
             <div class="grid-1-5">
                 <p class="price-package">Ecommerce</p>
-                <p class="price-in-pounds"><span class="uppercase">£999</span></p>
+                <p class="price-in-pounds"><span class="uppercase">£749</span></p>
                 <p>Up to 5 pages</p>
                 <ul>
-                    <li>Everything from Basic</li>
+                    <li>Everything from Affordable</li>
                     <li>Google Analytics</li>
                     <li>Content Management</li>
                     <li>1 year Free Hosting</li>
@@ -59,15 +58,16 @@ echo '
             </div>
             <div class="grid-1-5">
                 <p class="price-package">Ecommerce &amp; SEO</p>
-                <p class="price-in-pounds"><span class="uppercase">£1299</span></p>
+                <p class="price-in-pounds"><span class="uppercase">£1049</span></p>
                 <p>Up to 10 pages</p>
                 <ul>
-                    <li>Everything from Basic</li>
+                    <li>Everything from Affordable</li>
                     <li>Google Analytics</li>
                     <li>Content Management</li>
                     <li>2 year Free Hosting</li>
                     <li>Digital Visibility</li>
                     <li>Slideshow Animation</li>
+                    <li>Special Animations</li>
                 </ul>
                 <a class="button-pricing" href="tel:07794922161">Call us</a>
             </div>
--- 8.footer/footer.php
+++ 8.footer/footer.php
@@ -1,74 +1,78 @@
-<?php
-
-echo '
-
 <footer>
-        <div class="row" id="footer">
-            <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3 footer-small" itemscope itemtype="http://schema.org/LocalBusiness">
-                <h4>GET IN TOUCH</h4>
+    <div class="row" id="footer">
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3 footer-small">
+            <h4>GET IN TOUCH</h4>
+            <p>Brilliant Web Design</p>
+            <p><i class="fas fa-mobile-alt"></i><a href="tel:07794922161"> 07794922161</a></p>
 
-                <p><i class="fas fa-mobile-alt"></i><span itemprop="telephone" content="+447794922161"><a href="tel:07794922161"> 07794922161</a></span></p>
+            <p>
+                <i class="far fa-envelope"></i>
+                <a href="mailto:info@brilliantwebdesign.co.uk">info@brilliantwebdesign.co.uk</a>
+            </p>
 
-                <p><i class="far fa-envelope"></i> <span itemprop="email"><a href="mailto:info@brilliantwebdesign.co.uk">info@brilliantwebdesign.co.uk</a></span></p>
+            <p>
+                <i class="fas fa-map-marked-alt"></i>
+                <span>10 Springfield Rd</span>,
+                <span>Halesowen</span>
+                <span>B62 8JY</span>
+            </p>
 
-                <p><i class="fas fa-map-marked-alt"></i> <span itemprop="address" itemscope itemtype="http://schema.org/PostalAddress"><span itemprop="streetAddress">10 Springfield Rd</span>, <span itemprop="addressLocality">Halesowen</span> <span itemprop="postalCode">B62 8JY</span></span></p>
-
-                <p><i class="far fa-calendar-alt"></i> <span itemprop="openingHours" content="Mo-Fr 09:00-18:00">Mon-Fri, 9am-6pm</span></p>
-            </div>
-            <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3 footer-small">
-                <h4>QUICK LINKS</h4>
-                <p><a href="#">Website Design</a></p>
-                <p><a href="#section4">Responsive Design</a></p>
-                <p><a href="#seo">SEO Services</a></p>
-                <p><a href="https://www.seobility.net/en/seocheck/">SEO Checker</a></p>
-                <p><a href="http://www.webdesigndirectory.net" target="_blank">Web Design Directory</a></p>
-            </div>
-            <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3 footer-small">
-                <h4>AREAS WE COVER</h4>
-                <p><a href="web-design-birmingham.php"> <i class="fas fa-map-marker-alt"></i>Birmingham</a></p>
-                <p><a href="web-design-halesowen.php"><i class="fas fa-map-marker-alt"></i>Halesowen</a></p>
-                <p><a href="web-design-dudley.php"><i class="fas fa-map-marker-alt"></i>Dudley</a></p>
-                <p><a href="web-design-quinton.php"><i class="fas fa-map-marker-alt"></i>Quinton</a></p>
-                <p><a href="web-design-smethwick.php"><i class="fas fa-map-marker-alt"></i>Smethwick</a></p>
-                <p><a href="web-design-oldbury.php"><i class="fas fa-map-marker-alt"></i>Oldbury</a></p>
-            </div>
-
-            <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3 footer-small">
-                <h4>CLIENT LINKS</h4>
-                <p><a href="#particles-js">Home</a></p>
-                <p><a href="#your-local-design-company-section">About Us</a></p>
-                <p><a href="#what-we-offer">Services</a></p>
-                <p><a href="#responsive-web-design">Responsive</a></p>
-                <p><a href="#seo">SEO Optimisation</a></p>
-            </div>
+            <p><i class="far fa-calendar-alt"></i> <span>Mon-Fri, 9am-6pm</span></p>
         </div>
-        <div class="row" id="extra-websites">
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3 footer-small">
+            <h4>QUICK LINKS</h4>
+            <p><a href="#">Website Design</a></p>
+            <p><a href="#section4">Responsive Design</a></p>
+            <p><a href="#seo">SEO Services</a></p>
+            <p><a href="https://www.seobility.net/en/seocheck/">SEO Checker</a></p>
+            <p><a href="http://www.webdesigndirectory.net" target="_blank">Web Design Directory</a></p>
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3 footer-small">
+            <h4>AREAS WE COVER</h4>
+            <p><a href="web-design-birmingham.php"> <i class="fas fa-map-marker-alt"></i>Birmingham</a></p>
+            <p><a href="web-design-halesowen.php"><i class="fas fa-map-marker-alt"></i>Halesowen</a></p>
+            <p><a href="web-design-dudley.php"><i class="fas fa-map-marker-alt"></i>Dudley</a></p>
+            <p><a href="web-design-quinton.php"><i class="fas fa-map-marker-alt"></i>Quinton</a></p>
+            <p><a href="web-design-smethwick.php"><i class="fas fa-map-marker-alt"></i>Smethwick</a></p>
+            <p><a href="web-design-oldbury.php"><i class="fas fa-map-marker-alt"></i>Oldbury</a></p>
+        </div>
+
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-3 footer-small">
+            <h4>CLIENT LINKS</h4>
+            <p><a href="#particles-js">Home</a></p>
+            <p><a href="#your-local-design-company-section">About Us</a></p>
+            <p><a href="#what-we-offer">Services</a></p>
+            <p><a href="#responsive-web-design">Responsive</a></p>
+            <p><a href="#seo">SEO Optimisation</a></p>
+        </div>
+    </div>
+    <div class="row" id="extra-websites">
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-    <p>
-        <a href="web-design-willenhall.php">.</a>
-        <a href="web-design-wednesbury.php">.</a>
-        <a href="web-design-walsall.php">.</a>
-        <a href="web-design-tipton.php">.</a>
-        <a href="web-design-tamworth.php">.</a>
-        <a href="web-design-stourbridge.php">.</a>
-        <a href="web-design-sparkbrook.php">.</a>
-        <a href="web-design-solihull.php">.</a>
-        <a href="web-design-selly-oak.php">.</a>
-        <a href="web-design-rowley-regis.php">.</a>
-        <a href="web-design-minworth.php">.</a>
-        <a href="web-design-kidderminster.php">.</a>
-        <a href="web-design-frankley.php">.</a>
-        <a href="web-design-erdington.php">.</a>
-        <a href="web-design-edgbaston.php">.</a>
-        <a href="web-design-cradley-heath.php">.</a>
-        <a href="web-design-brierley-hill.php">.</a>
-        <a href="web-design-bilston.php">.</a>
-        <a href="web-design-bearwood.php">.</a>
-    </p>
+            <p>
+                <a href="web-design-willenhall.php">.</a>
+                <a href="web-design-wednesbury.php">.</a>
+                <a href="web-design-walsall.php">.</a>
+                <a href="web-design-tipton.php">.</a>
+                <a href="web-design-tamworth.php">.</a>
+                <a href="web-design-stourbridge.php">.</a>
+                <a href="web-design-sparkbrook.php">.</a>
+                <a href="web-design-solihull.php">.</a>
+                <a href="web-design-selly-oak.php">.</a>
+                <a href="web-design-rowley-regis.php">.</a>
+                <a href="web-design-minworth.php">.</a>
+                <a href="web-design-kidderminster.php">.</a>
+                <a href="web-design-frankley.php">.</a>
+                <a href="web-design-erdington.php">.</a>
+                <a href="web-design-edgbaston.php">.</a>
+                <a href="web-design-cradley-heath.php">.</a>
+                <a href="web-design-brierley-hill.php">.</a>
+                <a href="web-design-bilston.php">.</a>
+                <a href="web-design-bearwood.php">.</a>
+            </p>
         </div>
 
-        </div>
-    </footer>
-    
-    ';
+    </div>
+</footer>
+
+';
 ?>
